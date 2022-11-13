3e46b6101d8d47e82e61006b7717568d62edd76c
NoeVelaz <velazquezmoemi74@gmail.com>
2022-11-13 01:19:09 -0300

    Corregir script

--- BACK/Consultas a la BBDD en MySQL.sql
+++ BACK/Consultas a la BBDD en MySQL.sql
@@ -1,26 +1,26 @@
-INSERT INTO `insumos_vet`.`articulos` (`id_articulo`, `nombre`, `fecha_vto`, `stock_minimo`, `descripcion`) 
+INSERT INTO articulos(id_articulo, nombre, fecha_vto, stock_minimo, descripcion) 
 VALUES ('2','PIPETA', '2025-01-01', '10', 'antipulgas');
 
-SELECT * FROM insumos_vet.articulos
+SELECT * FROM articulos
 WHERE nombre="IVERMECTINA";
 
-INSERT INTO `insumos_vet`.`articulos_x_sucursal` (`id_articulo`, `id_estado`, `sucursal`, `cantidad`)
+INSERT INTO articulos_x_sucursal(id_articulo, id_estado, sucursal, cantidad)
 VALUES ('1', '1', '1', '1');
 
-INSERT INTO `insumos_vet`.`estados` (`id_estado`, `descripcion`) 
+INSERT INTO estados (id_estado, descripcion) 
 VALUES ('1', 'para_retiro');
 
-INSERT INTO `insumos_vet`.`sucursales` (`id_sucursal`, `nombre`, `domicilio`, `telefono`) 
+INSERT INTO sucursales(id_sucursal, nombre, domicilio, `telefono`) 
 VALUES ('1', 'Sarmiento', 'Av Colon 31', '3521 404040');
 
-UPDATE `insumos_vet`.`articulos` SET `fecha_vto` = '2024-12-01' WHERE (`id_articulo` = '1');
+UPDATE articulos SET fecha_vto = '2024-12-01' WHERE (id_articulo = '1');
 
-SELECT A.`nombre`, E.`descripcion`, S.`nombre`
-FROM `insumos_vet`.`articulos_x_sucursal` AS AxS
-JOIN`insumos_vet`.`articulos` AS A 
+SELECT A.nombre, E.descripcion, S.nombre
+FROM articulos_x_sucursal AS AxS
+JOIN articulos AS A 
 ON AxS.id_articulo=A.id_articulo 
-JOIN `insumos_vet`.`estados` AS E
+JOIN estados AS E
 ON AxS.id_estado=E.id_estado 
-JOIN `insumos_vet`.`sucursales` AS S
+JOIN sucursales AS S
 ON AxS.sucursal = S.id_sucursal;
 
