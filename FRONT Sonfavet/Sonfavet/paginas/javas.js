6d2a0be598150ac3be88ac878ad06bd18524d733
Luchodlf <Luciano_dlf@hotmail.com>
2022-11-15 11:18:29 -0300

    funcionidad javascript en el index

--- FRONT Sonfavet/Sonfavet/paginas/javas.js
+++ FRONT Sonfavet/Sonfavet/paginas/javas.js
@@ -1,16 +1,21 @@
-    let contras = ["Admi123", "admi1234", "Admi456", "admi456"]
-    let botton = document.querySelector("#boton")
-    
-    
-    botton.addEventListener("click", function(){
-      let contraseña = document.querySelector("#password").value
-    
-    if(contras.includes (contraseña)){
-        window.location.replace = ("./paginas/inicio.html") 
-        // Deberia redirigir a la página de inicio
-    }
-    else{
-        window.alert('Contraseña incorrecta')
-    }})
+function Cambiartitulo() {
+    const titulo = document.getElementById("tituloprincipal")
 
-    /* Como no se logró que redireccione a la página general del proyecto quedó descartado */
+    titulo.innerHTML = "GRACIAS POR VISITARNOS"
+    location.href="./paginas/inicio.html";
+    alert("usuario: Admin contraseña: 123")
+}
+
+function Validardatos() {
+
+    const Us = document.getElementById("usuario").value
+    const contra = document.getElementById("pass").value
+
+    if (Us == "Admin" && contra == "123") {
+        location.href="./paginas/inicio.html";
+        alert ('DEBERIA INGRESAR')    
+
+    } else {
+        alert("USUARIO y/o CONTRASEÑA INCONRRECTA")
+    }
+}
